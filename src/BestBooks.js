636b34f8ff9b6f30caa59086c8616bc68e6ba034
saraalshater <gleave.alshater@gmail.com>
2021-08-10 01:32:21 +0300

    done 12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,14 +3,67 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { withAuth0 } from '@auth0/auth0-react';
 import './BestBooks.css';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+
 
 class MyFavoriteBooks extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      status: '',
+      email: '',
+      image:'',
+    }
+  }
+
+  getBooks = async () => {
+    const { user, isAuthenticated } = this.props.auth0
+    if (isAuthenticated) {
+      let url = `http://localhost:3001/books`;
+      const paramObj = {
+        params: {
+          name: user.email,
+        },
+      };
+      axios
+        .get(url, paramObj)
+        .then((results) => {
+          this.setState({
+            book: results.data,
+            // image:results.data
+          });
+
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+
+  
+  componentDidMount() {
+    this.getBooks();
+  }
+
   render() {
 
     const { isAuthenticated } = this.props.auth0;
 
+  
+
 
     return(
+
+
+      
       <>
       {isAuthenticated && 
         (
@@ -23,6 +76,25 @@ class MyFavoriteBooks extends React.Component {
       </Jumbotron>
       
          ) }
+          <Carousel>
+          {this.state.book &&
+            this.state.book.map((item) => {
+              return (
+                <Carousel.Item interval={1000}>
+                  <img
+                    className="d-block w-100"
+                    src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_813319932_383768.jpg"
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+        </Carousel>
+
       </>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 ReactDOM.render(
   <Auth0Provider
-    domain="sara-alshater.us.auth0.com"
-    clientId="UIE3EqXvd9KyM9QA4Qk5KkXHz866Mm19"
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_KEY}
     redirectUri={window.location.origin}
   >
     <App />
